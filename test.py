ae219391f28a24317c46c2e2f9c80b38455015d4
Aswah-Malik <122225605+Aswah-Malik@users.noreply.github.com>
2023-03-06 00:55:28 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,40 +1,20 @@
-import unittest
-from unittest.mock import Mock
+import pytest
 from make import MyClass
 
-class TestTranslate(unittest.TestCase):
-    def setUp(self):
-        self.my_class = MyClass()
-        self.my_class.session = Mock()
-        self.my_class.api_url = "https://example.com"
-        self.my_class.headers = {"Content-Type": "application/json"}
-        self.my_class.data = {"model": "text-to-text", "prompt": "", "temperature": 0.8}
-
-    def test_translate_successful(self):
-        # Mock the response from the server
-        self.my_class.session.post.return_value.ok = True
-        self.my_class.session.post.return_value.json.return_value = {
-            "choices": [{"text": "你好"}]
-        }
-
-        # Call the function with some text to translate
-        result = self.my_class.translate("hello")
-
-        # Check that the result is correct
-        self.assertEqual(result, "你好")
-        self.assertEqual(
-            self.my_class.data["prompt"], "Please help me to translate，`hello` to Chinese"
-        )
-
-    def test_translate_failure(self):
-        # Mock a failure response from the server
-        self.my_class.session.post.return_value.ok = False
-
-        # Call the function with some text to translate
-        result = self.my_class.translate("hello")
-
-        # Check that the result is the original text (no translation)
-        self.assertEqual(result, "hello")
-        self.assertEqual(
-            self.my_class.data["prompt"], "Please help me to translate，`hello` to Chinese"
-        )
+class TestMyClass:
+    @pytest.fixture
+    def my_class(self):
+        return MyClass()
+    
+    def test_translate(self, my_class):
+        text = "Hello"
+        expected_output = "你好"
+        assert my_class.translate(text) == expected_output
+        
+        text = "Goodbye"
+        expected_output = "再见"
+        assert my_class.translate(text) == expected_output
+        
+        text = ""
+        expected_output = ""
+        assert my_class.translate(text) == expected_output
