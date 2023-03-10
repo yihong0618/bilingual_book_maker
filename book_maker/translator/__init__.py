939024ca4d86da7733fbbcd36ec38a86c5aa1dbb
yihong0618 <zouzou0208@gmail.com>
2023-03-10 15:33:51 +0800

    fix: lint

--- book_maker/translator/__init__.py
+++ book_maker/translator/__init__.py
@@ -1,6 +1,6 @@
 from book_maker.translator.chatgptapi_translator import ChatGPTAPI
-from book_maker.translator.gpt3_translator import GPT3
 from book_maker.translator.google_translator import Google
+from book_maker.translator.gpt3_translator import GPT3
 
 MODEL_DICT = {
     "chatgptapi": ChatGPTAPI,
