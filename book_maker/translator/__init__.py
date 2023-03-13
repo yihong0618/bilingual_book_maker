839c2ce6ad7c65a728ad06e9123387cec25ab5f0
Voyageyang <45915518+Voyageyang@users.noreply.github.com>
2023-03-13 20:49:23 +0800

    (#93) feat: add caiyun translator (#160)
    * feat: add caiyun translator
    
    * format code and update README-CN.md
    
    * fix: add caiyun_key args
    
    * fix: add raise
    
    ---------
    
    Co-authored-by: yihong0618 <zouzou0208@gmail.com>

--- book_maker/translator/__init__.py
+++ book_maker/translator/__init__.py
@@ -1,10 +1,12 @@
 from book_maker.translator.chatgptapi_translator import ChatGPTAPI
 from book_maker.translator.google_translator import Google
 from book_maker.translator.gpt3_translator import GPT3
+from book_maker.translator.caiyun_translator import Caiyun
 
 MODEL_DICT = {
     "chatgptapi": ChatGPTAPI,
     "gpt3": GPT3,
-    "google": Google
+    "google": Google,
+    "caiyun": Caiyun
     # add more here
 }
