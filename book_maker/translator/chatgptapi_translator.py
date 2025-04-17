e83d319eb1eb8440069cf2e95a29adeb45b7cffd
cxumol <cxumol@users.noreply.github.com>
2025-04-17 01:21:23 -0700

    decrease length_retry to 2

--- book_maker/translator/chatgptapi_translator.py
+++ book_maker/translator/chatgptapi_translator.py
@@ -153,7 +153,7 @@ class ChatGPTAPI(Base):
         # TODO work well or exception finish by length limit
         # Check if content is not None before encoding
         t_text = ""
-        max_len_retry= 32
+        max_len_retry= 2
         for len_retry in range(max_len_retry):
             cur_content = completion.choices[0].message.content
             cur_content = re.sub(r'<think>.*?</think>','',cur_content,flags=re.S) #r1-like things
